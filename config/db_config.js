4a261b21b73935c136ca2858dbd8ede67bfff3ff
kenedyivan <kenedyakenaivan@gmail.com>
2018-09-24 16:57:39 +0300

    Untracking config directory

--- config/db_config.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'phpmyadmin',
-    password: '123!@#QWEasd',
-    database: 'vehcurity_db'
-});
-
-module.exports = connection;
